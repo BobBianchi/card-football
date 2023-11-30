c2414deadb56671ad89e3c4686aeafd787d3de07
Bob Bianchi <rbianx@gmail.com>
2023-11-30 17:20:57 -0500

    added roll boxes and started gameboard

--- card-football-frontend/src/Component/RollBox.js
+++ card-football-frontend/src/Component/RollBox.js
@@ -1,12 +1,15 @@
-export default function RollBox({result}) {
+export default function RollBox({ result }) {
     return (
         <div
             style={{
                 border: "1px solid black",
                 height: "100px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
             }}
         >
-            {result}
+            <h3>{result}</h3>
         </div>
-    )
+    );
 }
--- card-football-frontend/src/Pages/GameBoardPage.js
+++ card-football-frontend/src/Pages/GameBoardPage.js
@@ -4,37 +4,67 @@ import Dice from "../Component/Dice";
 import RollBox from "../Component/RollBox";
 
 export default function GameBoardPage() {
-    const [ltResult, setLtResult] = useState([null, null, null, null, null]);
+    const oPositions = ["lt", "lg", "c", "rg", "rt"];
+    const dPositions = ["ledge", "lde", "dt", "rde", "redge"];
+    const [dLineResult, setDLineResult] = useState({
+        ledge: null,
+        lde: null,
+        dt: null,
+        rde: null,
+        redge: null,
+    });
+    const [oLineResult, setOLineResult] = useState({
+        lt: null,
+        lg: null,
+        c: null,
+        rg: null,
+        rt: null,
+    });
     const handleSnap = () => {
         console.log("hut hut");
-        setLtResult(Math.floor(Math.random() * 6) + 1);
+        const newOLineResult = { ...oLineResult };
+        const newDLineResult = { ...dLineResult };
+        oPositions.forEach((pos) => {
+            newOLineResult[pos] = Math.floor(Math.random() * 6) + 1;
+        });
+        setOLineResult(newOLineResult);
+        dPositions.forEach((pos) => {
+            newDLineResult[pos] = Math.floor(Math.random() * 4) + 1;
+        })
+        setDLineResult(newDLineResult);
     };
     useEffect(() => {
-        console.log("ltResult chaged: ", ltResult);
-    }, [ltResult]);
+        console.log("oLineResult changed: ", oLineResult);
+    }, [oLineResult]);
+    useEffect(() => {
+        console.log("dLineResult changed: ", dLineResult);
+    }, [dLineResult]);
     return (
         <div className="container">
             <div className="row" id="opponent-board">
                 <div className="container">
                     <div className="row">
                         <div className="col" id="lt">
+                            {dLineResult != null && (
+                                <RollBox result={dLineResult.ledge} />
+                            )}
                             <h3>EDGE</h3>
                         </div>
                         <div className="col" id="lg">
+                        {dLineResult != null && <RollBox result={dLineResult.lde} />}
                             <h3>DE</h3>
-                            <p></p>
                         </div>
                         <div className="col" id="c">
+                        {dLineResult != null && <RollBox result={dLineResult.dt} />}
                             <h3>DT</h3>
-                            <p></p>
                         </div>
                         <div className="col" id="rg">
+                        {dLineResult != null && <RollBox result={dLineResult.rde} />}
                             <h3>DE</h3>
-                            <p></p>
                         </div>
                         <div className="col" id="rt">
+                        {dLineResult != null && <RollBox result={dLineResult.redge} />}
                             <h3>EDGE</h3>
-                            <p></p>
                         </div>
                     </div>
                 </div>
@@ -45,20 +75,33 @@ export default function GameBoardPage() {
                     <div className="row">
                         <div className="col" id="lt">
                             <h3>LT</h3>
-                            {ltResult != null && <RollBox result={ltResult} />}
+                            {oLineResult != null && (
+                                <RollBox result={oLineResult.lt} />
+                            )}
                         </div>
                         <div className="col" id="lg">
                             <h3>LG</h3>
-                            {ltResult != null && <RollBox result={ltResult} />}
+                            {oLineResult != null && (
+                                <RollBox result={oLineResult.lg} />
+                            )}
                         </div>
                         <div className="col" id="c">
                             <h3>C</h3>
+                            {oLineResult != null && (
+                                <RollBox result={oLineResult.lg} />
+                            )}
                         </div>
                         <div className="col" id="rg">
                             <h3>RG</h3>
+                            {oLineResult != null && (
+                                <RollBox result={oLineResult.lg} />
+                            )}
                         </div>
                         <div className="col" id="rt">
                             <h3>RT</h3>
+                            {oLineResult != null && (
+                                <RollBox result={oLineResult.lg} />
+                            )}
                         </div>
                     </div>
                 </div>
